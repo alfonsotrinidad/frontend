333398a062b6bd9e8fb9ac12225d3cb7eb9d043d
alfonsotrinidad <43577249+alfonsotrinidad@users.noreply.github.com>
2020-08-30 13:03:06 -0500

    treinttarde

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,12 @@ import { ProductsService} from './services/products.service';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { FilterPipe } from './pipes/filter.pipe'
 
+import { MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,13 +29,19 @@ import { FilterPipe } from './pipes/filter.pipe'
     ProductsComponent,
     SignupComponent,
     ProductListComponent,
-    FilterPipe
+    FilterPipe,
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatSelectModule
   ],
   providers: [
     AuthService,
